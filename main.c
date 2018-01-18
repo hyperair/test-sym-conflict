e1e7ade2b5c93b28db77b5245ebfff067c221376
Chow Loong Jin <hyperair@debian.org>
2018-01-18 16:13:29 +0800

    Handle errors from dl

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 static void callhello (const char *filename)
 {
-    void *handle = dlopen (filename, RTLD_LOCAL);
+    void *handle = dlopen (filename, RTLD_NOW | RTLD_LOCAL);
+
+    if (!handle) {
+        fprintf (stderr, "%s\n", dlerror ());
+        exit (EXIT_FAILURE);
+    }
 
     typedef void (*helloworld_func) ();
     helloworld_func hi = dlsym (handle, "helloworld");
+
+    if (!hi) {
+        fprintf (stderr, "%s\n", dlerror ());
+        exit (EXIT_FAILURE);
+    }
+
     hi ();
 
     dlclose (handle);
